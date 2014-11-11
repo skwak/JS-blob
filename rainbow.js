acf16d7eb5b2e61b54f1a29cf73a71730627c135
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-10 16:50:41 -0800

    finish to do

---
+++ rainbow.js
@@ -0,0 +1,15 @@
+// Rainbow
+// weight 16
+// breed beagle
+// likes fetching balls
+
+var rainbow = {
+  name: "Rainbow",
+  weight: 16,
+  breed: "beagle",
+  likesFetching: true,
+  walkTimes: ["8am", "1pm", "9pm"],
+  bark: function bark() {
+    console.log("woof woof!");
+  }
+};
---
+++ todo.js
@@ -0,0 +1,30 @@
+// Create a to do object, with the following properties:
+//
+// a task(a string) - a description of the thing to do
+// who(a string) - the name of a person to do it
+// done(a boolean) - is the task done or not?
+// isDone (a method) - get the value of done
+// setDone(a method) - set the value of done
+//
+// create 2 or 3 of these to do objects
+// if that gets done, create an array of to-do objects
+
+var toDo = {
+  task: String,
+  who: String,
+  done: Boolean,
+  getDone: function() {
+    if (this.done === true) {
+      return true;
+    }
+    elsif (this.done === false); {
+      return false;
+    }
+  },
+  setDone: function(boolean) {
+    this.done = boolean;
+  }
+};
+
+toDo.task = "wash dishes";
+toDo.setDone(true);
